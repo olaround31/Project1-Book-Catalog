c3c43a6184622ad22e3dd693f020a0b0b9927745
JohnMichaelOLavidez <Olaround31@gmail.com>
2023-07-30 14:09:30 -0400

    project complete

--- main.js
+++ main.js
@@ -1,78 +1,75 @@
-// Fetch the JSON data from db.json using Fetch API
-fetch('db.json')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        window.database = data;
-        performSearch();
-    })
-    .catch(function(error) {
-        console.error("Error fetching JSON data:", error);
-    });
+// Sample book data (Replace this with your actual book data)
+const bookData = [
+  {
+    title: 'Book 1',
+    author: 'Author A',
+    genre: 'Fiction',
+    year: 2020,
+  },
+  {
+    title: 'Book 2',
+    author: 'Author B',
+    genre: 'Fantasy',
+    year: 2018,
+  },
+  // Add more books as needed
+];
 
-// Attach the search function to the form's submit event
-var searchForm = document.getElementById("searchForm");
-searchForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    performSearch();
+// Function to display books in the catalog
+function displayBooks(books) {
+  const catalogContainer = document.getElementById('bookCatalog');
+  catalogContainer.innerHTML = '';
+
+  books.forEach((book) => {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.innerHTML = `
+      <h2>${book.title}</h2>
+      <p>Author: ${book.author}</p>
+      <p>Genre: ${book.genre}</p>
+      <p>Year: ${book.year}</p>
+    `;
+    catalogContainer.appendChild(bookElement);
+  });
+}
+
+// Initial display of all books when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  displayBooks(bookData);
 });
 
-document.addEventListener("keydown", function (event) {
-  handleKeydown(event);
+// Function to filter books by genre
+function filterBooksByGenre(genre) {
+  const filteredBooks = bookData.filter((book) => book.genre === genre);
+  displayBooks(filteredBooks);
+}
+
+// Function to search for books by title or author
+function searchBooks(query) {
+  const lowercaseQuery = query.toLowerCase();
+  const matchedBooks = bookData.filter(
+    (book) =>
+      book.title.toLowerCase().includes(lowercaseQuery) ||
+      book.author.toLowerCase().includes(lowercaseQuery)
+  );
+  displayBooks(matchedBooks);
+}
+
+// Event listeners for filtering and searching
+document.getElementById('filterByGenre').addEventListener('change', (event) => {
+  const selectedGenre = event.target.value;
+  if (selectedGenre === 'all') {
+    displayBooks(bookData); // Show all books when 'all' is selected
+  } else {
+    filterBooksByGenre(selectedGenre);
+  }
 });
 
-performSearch();
-
-const fetchCheck = async () => {
-    const response = await fetch("http://localhost:3000/images/1").then(r=>r.json())
-    console.log(response)
-    return response
+document.getElementById('searchInput').addEventListener('input', (event) => {
+  const searchQuery = event.target.value.trim();
+  if (searchQuery === '') {
+    displayBooks(bookData); // Show all books when the search field is empty
+  } else {
+    searchBooks(searchQuery);
   }
-  
-  fetchCheck()
-  
-  // A method is a function declared
-  // from an object with dot notation.
-  
-  // select elements on the HTML page.
-  // .getElementsByTagName looks for everything on the page
-  // that has an HTML tag of whatever is in the perentheses:
-  const body = document.getElementsByTagName("body")[0];
-  const main = document.getElementsByTagName("main")[0];
-  
-  // .getElementById looks for HTML elements with an "id"
-  // of whatever is in the perentheses.
-  const input = document.getElementById("input");
-  const myName = document.getElementById("name");
-  const pageBg = document.getElementById("pageBg");
-  
-  const cardBg = document.getElementById("cardBg");
-  const button = document.getElementById("button");
-  
-  // add click event listener to pageBg change button
-  pageBg.addEventListener("click", function () {
-    body.style.backgroundColor = randomColor();
-  });
-  
-  // add click event listener to cardBg change button
-  cardBg.addEventListener("click", function () {
-    main.style.backgroundColor = randomColor();
-  });
-  
-  // add click event to "Change it" button
-  button.addEventListener("click", function () {
-    myName.textContent = input.value;
-    input.value = "";
-  });
-  
-  // This is a function.
-  // Functions perform actions.
-  // This function returns a random color.
-  function randomColor() {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-  
-    return `rgba(${r}, ${g}, ${b}, 1)`;
-  }
+});
